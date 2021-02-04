705c2d09a8e251387bab6ac3e401ae38093d8fa7
Ciaddie <ciaddiebb@gmail.com>
2021-02-03 19:43:25 -0700

    Implemented hard code with movies array

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,28 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    // var movies = [
-    //   {title: 'Mean Girls'},
-    //   {title: 'Hackers'},
-    //   {title: 'The Grey'},
-    //   {title: 'Sunshine'},
-    //   {title: 'Ex Machina'},
-    // ];
+    var movies = [
+      {title: 'Mean Girls'},
+      {title: 'Hackers'},
+      {title: 'The Grey'},
+      {title: 'Sunshine'},
+      {title: 'Ex Machina'},
+    ];
     return(
     <div>
       <h1>MovieList</h1>
       <ul>
-        <li>Mean Girls</li>
-        <li>Hackers</li>
-        <li>The Grey</li>
-        <li>Sunshine</li>
-        <li>Ex Machina</li>
+        <li>{movies[0].title}</li>
+        <li>{movies[1].title}</li>
+        <li>{movies[2].title}</li>
+        <li>{movies[3].title}</li>
+        <li>{movies[4].title}</li>
       </ul>
     </div>
   )}
 }
+
+
 
 export default App;
 

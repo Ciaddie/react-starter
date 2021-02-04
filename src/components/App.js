65ba18a35a2fc82db1c737861653b222b0a9597b
Ciaddie <ciaddiebb@gmail.com>
2021-02-03 19:30:55 -0700

    Hard coded without using the movie list array

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
+    // var movies = [
+    //   {title: 'Mean Girls'},
+    //   {title: 'Hackers'},
+    //   {title: 'The Grey'},
+    //   {title: 'Sunshine'},
+    //   {title: 'Ex Machina'},
+    // ];
     return(
     <div>
-      Starter App component - hello there
+      <h1>MovieList</h1>
+      <ul>
+        <li>Mean Girls</li>
+        <li>Hackers</li>
+        <li>The Grey</li>
+        <li>Sunshine</li>
+        <li>Ex Machina</li>
+      </ul>
     </div>
   )}
 }
 
 export default App;
+
